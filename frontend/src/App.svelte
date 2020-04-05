be432fd6ebb0b14c4ac7de56af8670f9114dfa72
shifat514 <shifat514@gmail.com>
2020-04-06 01:25:35 +0600

    image added

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -2,12 +2,14 @@
 	import {Router,link,Route} from "svelte-routing"
 	import login from './components/login.svelte' 
 	import register from './components/register.svelte' 
+    import homepage from './components/homepage.svelte'
 </script>
 
 
 
 <Router>
 <Route path="/login" exact component={login}></Route>  
-<Route path="/register" exact component={register}></Route>  
+<Route path="/register" exact component={register}></Route> 
+<Route path="/homepage" exact component={homepage}></Route> 
 <!-- exact er kaaj ki?? -->
 </Router>
