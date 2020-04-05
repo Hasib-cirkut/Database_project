281e67bfffed452f3c8e61dc856174dd419005a4
shifat514 <shifat514@gmail.com>
2020-04-06 00:37:15 +0600

    added

---
+++ frontend/src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+	import {Router,link,Route} from "svelte-routing"
+	import login from './components/login.svelte' 
+	import register from './components/register.svelte' 
+</script>
+
+
+
+<Router>
+<Route path="/login" exact component={login}></Route>  
+<Route path="/register" exact component={register}></Route>  
+<!-- exact er kaaj ki?? -->
+</Router>
---
+++ frontend/src/components/register.svelte
@@ -0,0 +1,91 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RESGISTER FORM</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row justify-content-center align-items-center">
+            <div class="col-12 ">
+                <form class="form-container">
+                   
+                    <div class="form-group">
+                        <h1 class="header">REGISTER</h1> 
+                            <div class="col">
+                              <input type="text" bind:value={first_name} class="form-control" placeholder="First name">
+                            </div>
+            
+                            <div class="col">
+                              <input type="text" bind:value={last_name} class="form-control" placeholder="Last name">
+                            </div>
+                        <div class="col">
+                            <input type="text" bind:value={address} class="form-control" placeholder="Address">
+                          </div>
+                      <label for="exampleInputEmail1">Email address</label>
+                      <input type="email" bind:value={email} class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                      <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                    <!-- </div>
+                    <div class="form-group"> -->
+                      <label for="exampleInputPassword1">Password</label>
+                      <input type="password" bind:value={password}  class="form-control" id="exampleInputPassword1">
+                    </div>
+                    <div class="form-group form-check">
+                      <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                      <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                    </div>
+                    <button type="submit" class="btn buttonstyle">Submit</button>
+                  </form>
+            
+               </div>
+            </div>
+        </div>
+</body>
+
+
+<style>
+
+.form-container{
+    background: #fff;
+    padding: 30px;
+    border-radius: 7px;
+    box-shadow: 10px 10px 30px lightgrey;
+    margin-top: 150px;
+}
+
+.buttonstyle{
+     background-color: rgb(11, 118, 206);
+     color: #fff;
+ }
+ .header{
+     font-family: inherit;
+     font-style: bold;
+     text-shadow: 2px;
+     align-items: center;
+     margin-left :10px;
+ }
+.form-group{
+    margin: 20px;
+}
+
+
+</style>
+
+
+<script>
+let first_name=''
+let last_name=''
+let address=''
+let email=''
+let password=''
+
+function handleForm(event){
+    event.preventDefault()
+    console.log(first_name)
+    console.log(last_name)
+    console.log(address)
+    console.log(email)
+    console.log()
+}
+
+
+</script>
